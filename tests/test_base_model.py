019156a130be43ae3bb9f6227dac5279f6f6c273
NicoV00 <6328@holbertonstudents.com>
2023-10-24 13:07:40 -0700

    fixed tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -5,9 +5,6 @@ import unittest
 from models.base_model import BaseModel
 
 
-class Testmodel(unittest.TestCase):
-    """Testing base model class"""
-
 class test_class_base(unittest.TestCase):
     """class for testing class base model"""
     """Global variables"""
@@ -16,7 +13,6 @@ class test_class_base(unittest.TestCase):
     def test__str__(self):
         """Test __str__"""
         self.assertEqual(str(self.test)[:11], "[BaseModel]")
-
     def test_to_dict(self):
         """test_to_dict """
         bm = BaseModel()
@@ -25,6 +21,39 @@ class test_class_base(unittest.TestCase):
         self.assertTrue(type(dic['created_at']) is str)
         self.assertTrue(type(dic['updated_at']) is str)
 
+    def test_id(self):
+        """Test the id attribute."""
+        test_dict = {}
+        for index in range(1, 101):
+            test_dict[f"model{index}"] = BaseModel()
+        for index, value in enumerate(test_dict.values(), 1):
+            self.assertNotEqual(value.id, test_dict.get(
+                f"model{index + 1}", self.test).id
+            )
+    def test_create_kwargs(self):
+        """ create class from dictionary """
+        my_model = BaseModel()
+        self.dict = my_model.to_dict()
+        self.kwargs = BaseModel(self.dict)
+        self.assertIsInstance(self.kwargs, BaseModel)
+
+    def test_update(self):
+        """ test update date """
+        my_model = BaseModel()
+        update_old = my_model.updated_at
+        my_model.save()
+        update_new = my_model.updated_at
+        self.assertTrue(update_old != update_new)
+
+    def test_created_at(self):
+        """Test create_at attribute."""
+        test_dict = {}
+        for index in range(1, 20):
+            test_dict[f"model{index}"] = BaseModel()
+        for index, value in enumerate(test_dict.values(), 1):
+            self.assertNotEqual(value.created_at, test_dict.get(
+                f"model{index + 1}", self.test).created_at
+            )
 
 if __name__ == "__main__":
     unittest.main()
