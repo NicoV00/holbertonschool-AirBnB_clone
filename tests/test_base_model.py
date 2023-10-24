83eda7b2474c350f23a122c6358f92efe4042a3e
NicoV00 <6328@holbertonstudents.com>
2023-10-24 12:54:21 -0700

    fixing requirements and uploading

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """test"""
+
 import unittest
 from models.base_model import BaseModel
 
@@ -7,9 +8,14 @@ from models.base_model import BaseModel
 class Testmodel(unittest.TestCase):
     """Testing base model class"""
 
+class test_class_base(unittest.TestCase):
+    """class for testing class base model"""
+    """Global variables"""
+    test = BaseModel()
+
     def test__str__(self):
         """Test __str__"""
-        self.assertEqual(str(test)[:11], "[BaseModel]")
+        self.assertEqual(str(self.test)[:11], "[BaseModel]")
 
     def test_to_dict(self):
         """test_to_dict """
