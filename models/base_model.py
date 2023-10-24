7ad094ff88937ca47a0c9a361ecc324e4efe1afc
NicoV00 <6328@holbertonstudents.com>
2023-10-24 12:22:36 -0700

    updatning base model

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,6 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize base model"""
-        from . import storage
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -20,7 +19,6 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """Return a string"""
