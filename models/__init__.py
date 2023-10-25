589f5158f888273f9302180bdd63009c5ad83a71
SantiagoleitasIbarra <6769@holbertonstudents.com>
2023-10-25 02:48:42 +0000

    fixed bugs

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+# Import the FileStorage class from the models/engine/file_storage module
+from models.engine.file_storage import FileStorage
+
+# Create a unique FileStorage instance for the application
+storage = FileStorage()
+
+# Call the reload() method to load previously saved data from the JSON file
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Base model created"""
 
+
+from models import storage
 from datetime import datetime
 from uuid import uuid4
 
@@ -20,6 +22,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return a string"""
@@ -28,6 +31,7 @@ class BaseModel:
     def save(self):
         """Update model"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return dictionary"""
