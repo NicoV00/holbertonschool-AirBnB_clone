a1cb6d7d0a7f2f091761856dcbb29bb145b69e42
SantiagoleitasIbarra <6769@holbertonstudents.com>
2023-10-25 02:20:12 +0000

    added  csses that inherit fromBaseModel

--- models/review.py
+++ models/review.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Import the BaseModel class"""
+
+
+from models.base_model import BaseModel
+
+"""Define the Review class that inherits from BaseModel"""
+class Review(BaseModel):
+    place_id = ""
+    user_id = ""
+    text = ""
--- models/state.py
+++ models/state.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Import the BaseModel class"""
+
+
+from models.base_model import BaseModel
+
+"""Define the State class that inherits from BaseModel"""
+class State(BaseModel):
+    name = ""
