6bd7025048668560aac50ae1c88ca3aeb1e157f7
SantiagoleitasIbarra <6769@holbertonstudents.com>
2023-10-25 01:58:57 +0000

    added command interpreter

--- console.py
+++ console.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Command interpreter for the AirBnB Clone project"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def help_quit(self):
+        print("Quit command to exit the program")
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
