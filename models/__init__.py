a1f796fe99e1d495895dc4d3253080ad577a1f3e
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-10-27 18:52:52 +0000

    fixed bug

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """FileStorage instance for your application"""
+
+
 from models.engine.file_storage import FileStorage
 
+classes = {
+        'BaseModel': BaseModel,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Place': Place,
+        'Review': Review,
+    }
 storage = FileStorage()
 storage.reload()
--- models/amenity.py
+++ models/amenity.py
@@ -7,3 +7,6 @@ from models.base_model import BaseModel
 """Define the Amenity class that inherits from BaseModel"""
 class Amenity(BaseModel):
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
